37fb22e03caaa2e49a2d101dccf2f85d7e0bf1e0
Benjamin Varga <ares9318@gmail.com>
2016-09-27 09:56:44 +0200

    Add Style #Tyden2

--- app/src/main/java/cz/muni/fi/pv256/movio2/uco_410371/MainActivity.java
+++ app/src/main/java/cz/muni/fi/pv256/movio2/uco_410371/MainActivity.java
@@ -2,15 +2,20 @@ package cz.muni.fi.pv256.movio2.uco_410371;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 /**
  * Created by Benjamin Varga on 20.9.2016.
  */
 public class MainActivity extends AppCompatActivity {
 
+    private static final String MOVIE_DB_API_KEY = BuildConfig.MOVIE_DB_API_KEY;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Log.i("API_KEY", MOVIE_DB_API_KEY);
     }
 }
